544ffde6c99652aebc9d122ffdbbf6229f7e9223
Mohammad Sajjad Hossain <32493764+sajjad385@users.noreply.github.com>
2022-11-03 10:38:22 +0600

    Accepted

---
+++ Birthday-Cake-Candles.php
@@ -0,0 +1,32 @@
+<?php
+
+/*
+ * Complete the 'birthdayCakeCandles' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY candles as parameter.
+ */
+
+function birthdayCakeCandles($candles) {
+    // Write your code here
+    sort($candles);
+    $tallest = end($candles);
+    return count(array_filter($candles, function ($item) use ($tallest) {
+        return $item == $tallest;
+    }));
+
+}
+
+$fptr = fopen(getenv("OUTPUT_PATH"), "w");
+
+$candles_count = intval(trim(fgets(STDIN)));
+
+$candles_temp = rtrim(fgets(STDIN));
+
+$candles = array_map('intval', preg_split('/ /', $candles_temp, -1, PREG_SPLIT_NO_EMPTY));
+
+$result = birthdayCakeCandles($candles);
+
+fwrite($fptr, $result . "\n");
+
+fclose($fptr);
